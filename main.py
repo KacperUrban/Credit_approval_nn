64bc462cdd75f7781259849b652a60c228e8ddcd
Kacper Urban <urbankacper613@gmail.com>
2022-06-18 17:13:21 +0200

    Create activation class and loss function

--- main.py
+++ main.py
@@ -32,6 +32,32 @@ class Dense(Layer):
         return input_gradient
 
 
+class Activation:
+    def __init__(self, activation, activation_prime):
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    def forward(self, input):
+        self.input = input
+        return self.activation(self.input)
+
+    def backward(self, output_gradient, learning_rate):
+        return np.multiply(output_gradient, self.activation_prime(self.input))
+
+
+class Tanh(Activation):
+    def __init__(self):
+        tanh = lambda x: np.tanh(x)
+        tanh_prime = lambda x: 1 - np.tanh(x) ** 2
+        super().__init__(tanh, tanh_prime)
+
+
+def mse(y_true, y_pred):
+    return np.mean(np.power(y_true - y_pred, 2))
+
+
+def mse_prime(y_true, y_pred):
+    return 2 * (y_pred - y_true) / np.size(y_true)
 
 
 # Wczytanie danych za pomocą biblioteki pandas
