697802a8912bd4433228b64808a21b86115b4770
Kacper Urban <urbankacper613@gmail.com>
2022-06-18 17:00:22 +0200

    Create layer and dense class

--- main.py
+++ main.py
@@ -2,5 +2,37 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.output = None
+
+    def forward(self, input):
+        pass
+
+    def backward(self, output_gradient, learning_rate):
+        pass
+
+
+class Dense(Layer):
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.randn(output_size, input_size)
+        self.bias = np.random.randn(output_size, 1)
+
+    def forward(self, input):
+        self.input = input
+        return np.dot(self.weights, self.input) + self.bias
+
+    def backward(self, output_gradient, learning_rate):
+        weights_gradient = np.dot(output_gradient, self.input.T)
+        input_gradient = np.dot(self.weights.T, output_gradient)
+        self.weights -= learning_rate * weights_gradient
+        self.bias -= learning_rate * output_gradient
+        return input_gradient
+
+
+
+
 # Wczytanie danych za pomocą biblioteki pandas
 credit = pd.read_csv("crx.data", header=None)
